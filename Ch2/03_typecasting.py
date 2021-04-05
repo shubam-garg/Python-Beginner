c6a1e0bcd0b262e5b6218b64eaad9bc809b4a12b
shubam-garg <81907680+shubam-garg@users.noreply.github.com>
2021-04-04 20:45:51 -0400

    Chapter_2

---
+++ Ch2/03_typecasting.py
@@ -0,0 +1,13 @@
+a = "123" # it is a string because no is in double quotes
+print (type(a))
+a = int(a) # string is converted into integer
+print(type(a))
+print (a+5)
+b = 1
+print(type(b))
+b = str(b)
+print(type(b))
+b = float(b)
+print(type(b))
+print(b)
+
