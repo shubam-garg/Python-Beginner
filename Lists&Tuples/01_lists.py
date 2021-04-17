ee72670cfe49320f096406dc5c2c4319854eaf91
shubam-garg <81907680+shubam-garg@users.noreply.github.com>
2021-04-16 21:58:13 -0400

    Ch

---
+++ Lists&Tuples/01_lists.py
@@ -0,0 +1,16 @@
+# create a list using []
+x=[1,3,6,2,4]
+# print the list with print() function
+print(x)
+#Access using index using x[0],x[1],x[2]
+print(x[2])
+# Change the value of list using
+x[0]=34
+print(x)
+# We can create a list with items of different types
+y=[22,"Shubi",False,2.0]
+print(y)
+# List slicing
+friends=["Roni","Joni","Bobby","Bhoot"]
+print(friends[0:3])
+print(friends[-3:])
---
+++ Lists&Tuples/02_listmethods.py
@@ -0,0 +1,14 @@
+# list sort
+l1=[1,31,5,27,19]
+l1.sort()
+print(l1)
+l1.reverse()
+print(l1)
+l1.append(45)# add in list use append() function
+print(l1)
+l1.insert(2,54)# in this function 2 defines index no to insert 54
+print(l1)
+l1.pop(2) # in this function value in index 2 will remove
+print(l1)
+l1.remove(19)# it will remove 19 no from list
+print(l1)
